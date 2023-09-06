e4920a76241a666d809647e97a7241103c933d4f
David <okekedavid1333@gmail.com>
2023-09-06 23:58:58 +0100

    update

--- src/components/mallamFx/MallamFx.tsx
+++ src/components/mallamFx/MallamFx.tsx
@@ -1,5 +1,62 @@
 export const MallamFx = () => {
+  const rates=[
+    {
+      name:"ETH/USD",
+      value:"231.543",
+      profit:true,
+    },
+    {
+      name:"ETH/USD",
+      value:"231.543",
+      profit:false,
+    },
+    {
+      name:"ETH/USD",
+      value:"231.543",
+      profit:true,
+    },
+    {
+      name:"ETH/USD",
+      value:"231.543",
+      profit:false,
+    },
+    {
+      name:"ETH/USD",
+      value:"231.543",
+      profit:false,
+    },
+    {
+      name:"ETH/USD",
+      value:"231.543",
+      profit:true,
+    },
+    {
+      name:"ETH/USD",
+      value:"231.543",
+      profit:false,
+    },
+    {
+      name:"ETH/USD",
+      value:"231.543",
+      profit:true,
+    },
+    
+
+  ]
   return (
-    <div>MallamFx</div>
+    <div className="w-[50vw] mx-auto p-12">
+      <h1 className="text-center mb-6 text-2xl">MallamFx</h1>
+      <div className="bg-pry w-full h-[40vh] mb-6">
+        <div>
+          {/* {rates &&  rates.map(rate)=>
+          (
+
+          )} */}
+        </div>
+      </div>
+      <div className="flex justify-center">
+      <button className="bg-btn p-2 text-white text-xs">See more</button>
+    </div>
+    </div>
   )
 }
