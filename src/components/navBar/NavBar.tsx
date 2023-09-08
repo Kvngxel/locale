48b59224c7024a71faf179399c8904d180b79c97
David <okekedavid1333@gmail.com>
2023-09-08 10:51:58 +0100

    Tailwind config update

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -3,9 +3,9 @@ import { Link } from "react-router-dom"
 export const NavBar = () => {
   return (
     <header className="flex justify-around bg-pry py-6 px-[15vw] items-center">
-        <h1 className="text-sec3 text-xl font-semibold">LOCALE</h1>
-        <input type="text" placeholder="Search Locale" className="text-center w-[20vw] py-2"/>
-        <nav className="flex gap-6">
+        <h1 className="text-sec3 text-base font-semibold">LOCALE</h1>
+        <input type="text" placeholder="Search Locale" className="text-center w-[20vw] py-2 text-2xs"/>
+        <nav className="flex gap-6 text-xs">
           <Link to={'/'}>Home</Link>
           <Link to={'/about'}>About</Link>
           <Link to={'/contact'}>Contact</Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -8,15 +8,13 @@ export default {
   ],
   theme: {
     fontSize: {
-      xs: ['12px', '16px'],
-      sm: ['14px', '20px'],
-      base: ['16px', '19.5px'],
-      lg: ['18px', '21.94px'],
-      xl: ['20px', '24.38px'],
-      '2xl': ['32px', '29.26px'],
-      '3xl': ['28px', '50px'],
-      '4xl': ['48px', '58px'],
-      '8xl': ['96px', '106px']
+      '2xs': ['12px', '16px'],
+      xs: ['15px', '16px'],
+      sm: ['18px', '20px'],
+      base: ['22px', '19.5px'],
+      lg: ['30px', '21.94px'],
+      xl: ['40px', '24.38px'],
+      '2xl': ['45px', '50px'],
     },
     extend: {
       fontFamily: {
@@ -26,9 +24,9 @@ export default {
       colors: {
         'pry': "#EEEEEE",
         "sec": "#B8D3AB",
-        "sec2": "#F8F8F8",
+        "sec2": "#F4F4F4",
         "sec3": "#26520A",
-        "btn": "#3A8218",
+        "btn": "#122D05",
       },
 
     },
