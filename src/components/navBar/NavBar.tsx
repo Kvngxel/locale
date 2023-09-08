8d28fba7b6cfa4fce43ee54de15093f8b214587e
kvngxel <exceleze45@gmail.com>
2023-09-08 10:21:31 +0100

    Test

--- src/components/navBar/NavBar.tsx
+++ src/components/navBar/NavBar.tsx
@@ -4,7 +4,7 @@ export const NavBar = () => {
         <h1 className="text-sec3 text-xl font-semibold">LOCALE</h1>
         <input type="text" placeholder="Search Locale" className="text-center w-[20vw] py-2"/>
         <nav className="flex gap-6">
-            <button onClick={() => window.location.href = `/`} >Home Page</button>
+            <button onClick={() => window.location.href = `/`} >Home</button>
             <button onClick={() => window.location.href = `/AboutUs/1`} >About</button>
             <button onClick={() => window.location.href = `/ContactUs/1`} type="submit">Contact</button>
         </nav>
