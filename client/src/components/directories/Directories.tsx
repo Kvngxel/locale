8d765bb020a57739cd5a5970f533da4adb0438a3
David <okekedavid1333@gmail.com>
2023-09-18 19:18:29 +0100

    converted Jsx file back to tsx

--- client/src/components/directories/Directories.tsx
+++ client/src/components/directories/Directories.tsx
@@ -1,32 +1,38 @@
-import React, { useEffect, useState } from "react";
 import { heroImg } from "../../assets/images";
+import { useEffect, useState } from "react";
 
+interface ApiItem {
+    address:string; 
+    created_at:string;
+    description:string; 
+    id:number;
+    name:string;
+    updated_at:string;
+}
 export const Directories = () => {
-  const [companies, setCompanies] = useState([]); // Initialize state with an empty array
+  const [data, setData] = useState<ApiItem[]>([]);
 
   useEffect(() => {
-    // Fetch data when the component mounts
-    fetchDirectories();
-  }, []);
-
-  const fetchDirectories = async () => {
-    try {
-      const response = await fetch("http://localhost:3000/api/directories");
-      if (!response.ok) {
-        throw new Error("Failed to get response");
+    const fetchData = async () => {
+      try {
+        const response = await fetch('http://localhost:3000/api/directories');
+        const apiData: ApiItem[] = await response.json();
+        setData(apiData);
+        console.log(apiData);
+      } catch (error) {
+        console.error('Error fetching data:', error);
       }
-      const data = await response.json();
-      setCompanies(data); // Update the state with the fetched data
-    } catch (error) {
-      console.error("Error:", error);
-    }
-  };
+    };
+
+    fetchData();
+  }, []);
 
   return (
     <div className="w-[70vw] mx-auto p-12 flex flex-col">
       <h1 className="text-lg text-sec4 self-center mb-20">LOCALE DIRECTORIES</h1>
       <div className="flex w-full flex-wrap justify-evenly gap-6 rounded gap-y-16">
-        {companies.map((company) => (
+
+        {data && data.map((company:ApiItem) => (
           <div key={company.id} className="relative basis-[40%] rounded-lg basis">
             <img src={heroImg} alt="" className="w-full rounded-[9px]" />
             <div className="absolute bottom-0 w-full h-[20%] items-center rounded-[9px] bg-sec flex justify-around">
@@ -36,6 +42,7 @@ export const Directories = () => {
           </div>
         ))}
       </div>
+      <div className="flex justify-center gap-6"><h1>{'<'}</h1> <h1>1 of 5</h1> <h1>{'>'}</h1></div>
     </div>
   );
 };
