e92fefacd942b64fcc20aa3fb7b5a6d69f324ed9
David <okekedavid1333@gmail.com>
2023-09-18 23:56:04 +0100

    Adding the next and previous buttons

--- client/src/components/directories/Directories.tsx
+++ client/src/components/directories/Directories.tsx
@@ -11,6 +11,75 @@ interface ApiItem {
 }
 export const Directories = () => {
   const [data, setData] = useState<ApiItem[]>([]);
+  const [first, setFirst] = useState(0);
+  // this is a template data to show the maximum and minimum are working
+   const data2=[
+    {
+      id:1,
+      name:"jokes",
+    },
+    {
+      id:2,
+      name:"jokes",
+    },
+    {
+      id:3,
+      name:"jokes",
+    },
+    {
+      id:4,
+      name:"jokes",
+    },
+    {
+      id:5,
+      name:"jokes",
+    },
+    {
+      id:6,
+      name:"jokes",
+    },
+    {
+      id:7,
+      name:"jokes",
+    },
+    {
+      id:8,
+      name:"jokes",
+    },
+    {
+      id:9,
+      name:"jokes",
+    },
+    {
+      id:10,
+      name:"jokes",
+    },
+    {
+      id:11,
+      name:"jokes",
+    },
+    {
+      id:12,
+      name:"jokes",
+    },
+    {
+      id:13,
+      name:"jokes",
+    },
+   ]
+
+  const increase = () =>{
+    if(first+6<data2.length){
+      setFirst(first+6);
+    }
+
+  } 
+  const reduce = () =>{
+    if(first>0){
+      setFirst(first-6);
+    }
+
+  } 
 
   useEffect(() => {
     const fetchData = async () => {
@@ -32,17 +101,18 @@ export const Directories = () => {
       <h1 className="text-lg text-sec4 self-center mb-20">LOCALE DIRECTORIES</h1>
       <div className="flex w-full flex-wrap justify-evenly gap-6 rounded gap-y-16">
 
-        {data && data.map((company:ApiItem) => (
+        {data2 && data2.slice(first,first+6).map((company) => (
           <div key={company.id} className="relative basis-[40%] rounded-lg basis">
             <img src={heroImg} alt="" className="w-full rounded-[9px]" />
             <div className="absolute bottom-0 w-full h-[20%] items-center rounded-[9px] bg-sec flex justify-around">
               <p className="text-sm">{company.name}</p>
+              <p className="text-sm">{company.id}</p>
               <p className="text-xs text-white">See more ...</p>
             </div>
           </div>
         ))}
       </div>
-      <div className="flex justify-center gap-6"><h1>{'<'}</h1> <h1>1 of 5</h1> <h1>{'>'}</h1></div>
+      <div className="flex justify-center gap-6 mt-6 items-center"><h1 className="bg-sec aspect-[] rounded py-2 px-4" onClick={reduce}>{'<'}</h1> <h1>1 of 5</h1> <h1 onClick={increase} className="bg-sec aspect-[] rounded py-2 px-4">{'>'}</h1></div>
     </div>
   );
 };
