a889cd4648635a58fac85f32156ecf14d7b11ada
kvngxel <exceleze45@gmail.com>
2023-09-21 08:57:59 +0100

    Made Team profiles responsive

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -22,7 +22,15 @@ export const About = () => {
           <h2 className="text-lg font-inter font-medium">Meet the Team</h2> 
         </div>
         <div className="flex flex-wrap justify-center mt-20 mb-40">
-          <div className="bg-sec2 text-center rounded-3xl w-72 mx-6 font-sans font-light text-sm py-4">
+          <div className="bg-sec2 text-center rounded-3xl w-72 mx-6 font-sans font-light text-sm py-4 mb-16">
+            <img src="https://media.istockphoto.com/id/1206439390/photo/silhouette-of-profile-guy-in-shirt-with-white-button-in-aqua-menthe-circle-on-black.webp?s=170667a&w=0&k=20&c=xIcUug38E_KlqT3HjxDYSQE7ZlMyV0y0vKmkXmr016U="
+                alt=""
+                className="rounded-full h-40 w-40 mx-auto my-5" />
+            <p className="pt-2 text-base">Excel</p>
+            <p className="pt-3">FullStack Developer</p>
+            <p className="py-4">Sports Loving Programmer</p>
+            <button className="bg-gray-200 hover:bg-gray-300 hover:text-white px-10 py-3 mb-2 mt-4">Github</button>
+          </div><div className="bg-sec2 text-center rounded-3xl w-72 mx-6 font-sans font-light text-sm py-4 mb-16">
             <img src="https://media.istockphoto.com/id/1206439390/photo/silhouette-of-profile-guy-in-shirt-with-white-button-in-aqua-menthe-circle-on-black.webp?s=170667a&w=0&k=20&c=xIcUug38E_KlqT3HjxDYSQE7ZlMyV0y0vKmkXmr016U="
                 alt=""
                 className="rounded-full h-40 w-40 mx-auto my-5" />
@@ -31,16 +39,7 @@ export const About = () => {
             <p className="py-4">Sports Loving Programmer</p>
             <button className="bg-gray-200 hover:bg-gray-300 hover:text-white px-10 py-3 mb-2 mt-4">Github</button>
           </div>
-          <div className="bg-sec2 text-center rounded-3xl w-72 mx-6 font-sans font-light text-sm py-4">
-            <img src="https://media.istockphoto.com/id/1206439390/photo/silhouette-of-profile-guy-in-shirt-with-white-button-in-aqua-menthe-circle-on-black.webp?s=170667a&w=0&k=20&c=xIcUug38E_KlqT3HjxDYSQE7ZlMyV0y0vKmkXmr016U="
-                alt=""
-                className="rounded-full h-40 w-40 mx-auto my-5" />
-            <p className="pt-2 text-base">Excel</p>
-            <p className="pt-3">FullStack Developer</p>
-            <p className="py-4">Sports Loving Programmer</p>
-            <button className="bg-gray-200 hover:bg-gray-300 hover:text-white px-10 py-3 mb-2 mt-4">Github</button>
-          </div>
-          <div className="bg-sec2 text-center rounded-3xl w-72 mx-6 font-sans font-light text-sm py-4">
+          <div className="bg-sec2 text-center rounded-3xl w-72 mx-6 font-sans font-light text-sm py-4 mb-16">
             <img src="https://media.istockphoto.com/id/1206439390/photo/silhouette-of-profile-guy-in-shirt-with-white-button-in-aqua-menthe-circle-on-black.webp?s=170667a&w=0&k=20&c=xIcUug38E_KlqT3HjxDYSQE7ZlMyV0y0vKmkXmr016U="
                 alt=""
                 className="rounded-full h-40 w-40 mx-auto my-5" />
