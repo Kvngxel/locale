6077b5e039971498246ec51de536531fcebc0598
David <okekedavid1333@gmail.com>
2023-09-27 15:58:49 +0100

    fix 9

--- client/src/components/navBar/Dropdown.tsx
+++ client/src/components/navBar/Dropdown.tsx
@@ -7,7 +7,7 @@ const Dropdown = () => {
   const handleLogout = () => {
     localStorage.removeItem("token");
     localStorage.removeItem("isComplete");
-    Navigate("/");
+    Navigate("/login");
   };
   useEffect(() => {
     setIsLoggedin(!!localStorage.getItem("token")); // Convert to boolean using double negation !!
