d15e65b4adc51f8cd1d40c80cdf6446874c0a993
David <okekedavid1333@gmail.com>
2023-09-27 15:15:53 +0100

    fix 8

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,67 +1,65 @@
-import { Home } from "./pages/home/Home";
-import { About } from "./pages/about/About";
-import { Contact } from "./pages/contact/Contact";
-import { Mallamfx } from "./pages/Mallamfx";
+import React, { useEffect, useState } from "react";
 import {
   BrowserRouter as Router,
   Routes,
   Route,
   Navigate,
 } from "react-router-dom";
+import { Home } from "./pages/home/Home";
+import { About } from "./pages/about/About";
+import { Contact } from "./pages/contact/Contact";
+import { Mallamfx } from "./pages/Mallamfx";
 import { Login } from "./pages/login/Login";
 import { UpdateProfile } from "./pages/profile/UpdateProfile";
 import { SignUp } from "./pages/signup/SignUp";
 import { Locale } from "./pages/locale/Locale";
 import { Profile } from "./pages/profile/Profile";
 import { UpdateLocale } from "./pages/locale/UpdateLocale";
-import { useEffect, useState } from "react";
 
 function App() {
   const [completeProfile, setCompleteProfile] = useState(true);
-  const [isLoggedIn, setIsLoggedin] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   useEffect(() => {
-    setIsLoggedin(!!localStorage.getItem("token")); // Convert to boolean using double negation !!
+    setIsLoggedIn(!!localStorage.getItem("token"));
     setCompleteProfile(!!localStorage.getItem("isComplete"));
-  }, [Navigate, localStorage]);
+  }, []);
 
   return (
-    <>
-      <Router>
-        <Routes>
-          <Route
-            path="/login"
-            element={isLoggedIn ? <Navigate to="/" /> : <Login />}
-          />
-          <Route
-            path="/signup"
-            element={isLoggedIn ? <Navigate to="/" /> : <SignUp />}
-          />
-          <Route
-            path="/"
-            element={
-              isLoggedIn ? (
-                completeProfile ? (
-                  <Home />
-                ) : (
-                  <Navigate to="updateprofile" />
-                )
-              ) : (
+    <Router>
+      <Routes>
+        <Route
+          path="/login"
+          element={isLoggedIn ? <Navigate to="/" /> : <Login />}
+        />
+        <Route
+          path="/signup"
+          element={isLoggedIn ? <Navigate to="/" /> : <SignUp />}
+        />
+        <Route
+          path="/"
+          element={
+            isLoggedIn ? (
+              completeProfile ? (
                 <Home />
+              ) : (
+                <Navigate to="updateprofile" />
               )
-            }
-          />
-          <Route path="/updateprofile" element={<UpdateProfile />} />
-          <Route path="/" element={<Home />} />
-          <Route path="/malamfx" element={<Mallamfx />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/locale" element={<Locale />} />
-          <Route path="/updatelocale" element={<UpdateLocale />} />
-          <Route path="*" element={"Error 404"} />
-        </Routes>
-      </Router>
-    </>
+            ) : (
+              <Home />
+            )
+          }
+        />
+        <Route path="/updateprofile" element={<UpdateProfile />} />
+        <Route path="/malamfx" element={<Mallamfx />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/profile" element={<Profile />} />
+        <Route path="/locale" element={<Locale />} />
+        <Route path="/updatelocale" element={<UpdateLocale />} />
+        <Route path="*" element= "Error 404" />
+      </Routes>
+    </Router>
   );
 }
 
