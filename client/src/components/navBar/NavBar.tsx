bd94bcfe23f9612811867808ab89f018992fb408
kvngxel <exceleze45@gmail.com>
2023-09-27 12:23:03 +0100

    fix 2

--- client/src/components/navBar/NavBar.tsx
+++ client/src/components/navBar/NavBar.tsx
@@ -25,8 +25,8 @@ export const NavBar = () => {
     <nav className="flex gap-7 text-nxl max-sm:hidden">
       <Link className="hover:text-blue-400 hover:font-semibold text-black custom-selection pt-5" to={'/login'}>Log in</Link>
       <Link className="bg-[#0d0c22] hover:bg-[#ffffff] rounded-full py-5 px-7 text-white hover:text-black font-semibold " to={'/signup'}>Sign Up</Link>
-      <button onClick={handleLogout} className="bg-[#0d0c22] hover:bg-[#ffffff] rounded-full py-5 px-7 text-white hover:text-black font-semibold " >Log out</button>       
-       
+      <button onClick={handleLogout} className="bg-[#0d0c22] hover:bg-[#ffffff] rounded-full
+        py-5 px-7 text-white hover:text-black font-semibold" >Log out</button>       
     </nav>
     <div className="hidden max-sm:block">
       <Dropdown />
